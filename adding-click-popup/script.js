924161ea399e6c1b29fb9c8b2ae0d71c2a3355f2
knaughton782 <kirsten.naughton@gmail.com>
2021-03-24 15:34:09 -0700

    positioned span/pop up elements

--- adding-click-popup/script.js
+++ adding-click-popup/script.js
@@ -1,9 +1,12 @@
 function magnify(imgID, zoom) {
-    var img, glass, w, h, bw;
+    var img, glass, w, h, bw, spot;
     img = document.getElementById(imgID);
+    // spot = querySelector('.spot');
+
     /*create magnifier glass:*/
     glass = document.createElement("DIV");
     glass.setAttribute("class", "img-magnifier-glass");
+
     /*insert magnifier glass:*/
     img.parentElement.insertBefore(glass, img);
     /*set background properties for the magnifier glass:*/
@@ -13,6 +16,7 @@ function magnify(imgID, zoom) {
     bw = 3;
     w = glass.offsetWidth / 2;
     h = glass.offsetHeight / 2;
+
     /* execute a function when someone moves the magnifier glass over the image */
     glass.addEventListener("mousemove", moveMagnifier);
     img.addEventListener("mousemove", moveMagnifier);
@@ -22,36 +26,52 @@ function magnify(imgID, zoom) {
     /* and with click */
     glass.addEventListener("click", moveMagnifier);
     img.addEventListener("click", moveMagnifier);
+    // spot.addEventListener("click", hideSpot);
+
     function moveMagnifier(e) {
         var pos, x, y;
         /* prevent any other actions that may occur when moving over the image */
         e.preventDefault();
+
         /* get the cursor's x and y positions */
         pos = getCursorPos(e);
         x = pos.x;
         y = pos.y;
+
         /* prevent the magnifier glass from being positioned outside the image */
         if (x > img.width - (w / zoom)) { x = img.width - (w / zoom); }
         if (x < w / zoom) { x = w / zoom; }
         if (y > img.height - (h / zoom)) { y = img.height - (h / zoom); }
         if (y < h / zoom) { y = h / zoom; }
+
         /* set the position of the magnifier glass */
         glass.style.left = (x - w) + "px";
         glass.style.top = (y - h) + "px";
+
         /* display what the magnifier glass "sees" */
         glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
     }
+
     function getCursorPos(e) {
         var a, x = 0, y = 0;
         e = e || window.event;
+
         /* get the x and y positions of the image */
         a = img.getBoundingClientRect();
+
         /* calculate the cursor's x and y coordinates, relative to the image */
         x = e.pageX - a.left;
         y = e.pageY - a.top;
+
         /* consider any page scrolling */
         x = x - window.pageXOffset;
         y = y - window.pageYOffset;
         return { x: x, y: y };
     }
+
+    // function hideSpot(spot) {
+    //     document.querySelector('.spot').addEventListener('click', () => {
+    //         document.querySelector('.spot').classList.add('spotHide');
+    //     });
+    // }
 }
